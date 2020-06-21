962ab0b1bf0a77e43292b9603072f9d746b64f74
Jordan <jordan7751@gmail.com>
2020-06-21 22:51:02 +0200

    implemented recycler view in FirstFragment.kt to show overview of available labels

--- app/src/main/java/com/example/flashycards/FirstFragment.kt
+++ app/src/main/java/com/example/flashycards/FirstFragment.kt
@@ -4,29 +4,90 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flashycards.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding
+    get() = _binding!!
+
+    private var allLabels: MutableList<String> = emptyList<String>().toMutableList()
+    private val viewAdapter: RecyclerView.Adapter<*> = MyAdapter(allLabels)
+    private val myObserver = Observer<List<String>> {
+        println("observer triggered labellist: ${it}")
+        allLabels.clear()
+        allLabels.addAll(it)
+        viewAdapter.notifyDataSetChanged()
+        println("set labellist: ${allLabels}")
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        binding.buttonFirst.setOnClickListener {
             val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("Testwörter1")
             findNavController().navigate(action)
         }
+
+
+        (activity as MainActivity).viewModel.allLabels.observe(viewLifecycleOwner, myObserver)
+        val viewManager: RecyclerView.LayoutManager = GridLayoutManager(context,2)
+        binding.recyclerviewCardpile.apply {
+            //setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        (activity as MainActivity).viewModel.allLabels.removeObserver(myObserver)
+    }
+
+
+    private class MyAdapter(private val myDataset: List<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+        class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val textView = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
+            return  MyViewHolder(textView)
+        }
+
+        override fun getItemCount(): Int = myDataset.size
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.textView.text = myDataset[position]
+
+            holder.itemView.setOnClickListener {
+                val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(myDataset[position])
+                it.findNavController().navigate(action)
+            }
+        }
+
+
     }
 }

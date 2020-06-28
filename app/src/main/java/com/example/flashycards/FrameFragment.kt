e741222f74d49b2d30be76a9e8e98d25f51241ea
Jordan <jordan7751@gmail.com>
2020-06-28 22:23:26 +0200

    add Binding in FrameFragment.kt

--- app/src/main/java/com/example/flashycards/FrameFragment.kt
+++ app/src/main/java/com/example/flashycards/FrameFragment.kt
@@ -4,30 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import com.example.flashycards.databinding.FragmentFrameBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class FrameFragment : Fragment() {
 
+    private var _binding: FragmentFrameBinding? = null
+    private val binding get() = _binding!!
+
     companion object{
-        lateinit var front :String
-        lateinit var back :String
+        lateinit var frame: FrameLayout
+        lateinit var front: String
+        lateinit var back: String
 
         fun  newInstance(front: String, back: String): FrameFragment {
             val frameFragment = FrameFragment()
-//            val fragment = CardFragment()
             val args = Bundle()
             args.putString("frontSide", front)
             args.putString("backSide", back)
             this.front = front
             this.back = back
+
             frameFragment.arguments = args
-//            frameFragment.childFragmentManager
-//                .beginTransaction()
-//                .add(R.id.frame, fragment)
-//                .commit()
             return frameFragment
         }
     }
@@ -37,6 +39,8 @@ class FrameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        _binding = FragmentFrameBinding.inflate(inflater, container, false)
+
         if(savedInstanceState == null) {
             front = arguments?.getString("frontSide") ?: "NOTFOUND"
             back = arguments?.getString("backSide") ?: "NOTFOUND"
@@ -44,17 +48,25 @@ class FrameFragment : Fragment() {
             childFragmentManager
                 .beginTransaction()
                 .add(R.id.frame, cardFragment)
-                .commit();
+                .commit()
         } else {
-            //front = savedInstanceState.getString("frontSide") ?: "EMPTY"
-            //back = savedInstanceState.getString("backSide") ?: "EMPTY"
             childFragmentManager
                 .beginTransaction()
                 .add(R.id.frame, CardFragment.newInstance(front, back))
-                .commit();
+                .commit()
         }
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frame, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        frame = binding.frame
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
